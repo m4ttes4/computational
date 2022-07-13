9313aebb1fcd8d3b24ec973a03df8938e6147f66
m4ttes4 <matteosap@hotmail.com>
2022-07-13 12:38:42 +0200

    grafici

--- progetto1/diffusione.py
+++ progetto1/diffusione.py
@@ -4,10 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #data = pd.read_csv(r'C:\Users\matteo\Desktop\computational\progetto2\sedov010.dat',header=None,sep='\s+') 
-data = pd.read_csv(r'C:\Users\matteo\Desktop\computational\progetto1\diff_sorg.dat',header=None,sep='\s+',
+data = pd.read_csv(r'C:\Users\matte\Desktop\computational\progetto1\time.dat',header=None,sep='\s+',
  comment='#', engine='python')
 #data =  pd.read_csv(r'C:\Users\matteo\Desktop\computational\progetto2\sedov000.dat',header=None,sep='\s+')
-#data2 = pd.read_csv(r'C:\Users\matteo\Desktop\computational\test1\shock_radius_winds2.dat',header=None,sep='\s+')
+data2 = pd.read_csv(r'C:\Users\matte\Desktop\computational\progetto1\troppo.dat',header=None,sep='\s+', 
+comment='#')
 
 labels = ['t = 2 Gyr', 't = 3 Gyr', 't = 5 Gyr', 't = 8 Gyr', 't = 10 Gyr']
 colors =['#f89441','#e56b5d','#cb4679','#a82296','#0c0887']
@@ -22,7 +23,8 @@ for i in range(3,8):
     ax.set_xscale('log')
 ax.plot(data[0], data[1], label='$Z_{fe}$ subcracted', color = 'black')
 ax.plot(data[0], data[2], label='$Z_{fe}$ observed', linestyle = '--',color = 'black')
-ax.set_xlabel('distance [Mpc]')
+ax.plot(data2[0], data2[1], color='green', label='25 Gyr', linestyle='-.')
+ax.set_xlabel('distance [Kpc]')
 ax.set_ylabel('$Z_{fe} / Z_{sol}$')
 plt.legend()
 fig.suptitle('source and diffusion')
