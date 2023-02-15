6c2468ad82e843755c86536850a1a64f75300c5c
m4ttes4 <matteosap@hotmail.com>
2023-02-15 16:57:14 +0100

    hh

---
+++ relazioni/Matteo_Sapori_computational/Perseus.f90
@@ -0,0 +1,627 @@
+
+
+MODULE DATA
+   
+    real*8 :: pi,cmpc,cmkpc,yr,boltz,mu,mp, guniv,msol
+    parameter (guniv=6.6720e-8)
+    parameter(pi=3.141592)
+    parameter(cmpc=3.085d18)
+    parameter(cmkpc=1000.*cmpc)
+    parameter(yr=3.156d7)
+    parameter(boltz=1.38d-16)
+    parameter(mu=0.61)
+    parameter(mp=1.67d-24)
+    parameter(msol= 1.989d33)
+    parameter(jmax=5000) 
+    
+ END MODULE DATA
+ 
+ 
+ 
+ 
+ program progetto1
+    use DATA
+ 
+ 
+    
+ implicit real*8 (a-h,o-z) !ogni variabile dalla a alla h e o alla z sono real*8 !! i l m n
+ real*8, dimension(jmax) :: r(jmax),        rr(jmax),           vol(jmax),  massaDM_num(jmax),&
+         dens_gas(jmax),    densDM(jmax),   massaDM_ana(jmax),  grav2(jmax),&
+         grvnfw(jmax),      lnd(jmax),      dens_gas_ana(jmax), M_star(jmax), &
+         pot_gal(jmax),     M_tot(jmax),    M_gas(jmax), num1(jmax), den1(jmax),&
+         den2(jmax),        T(jmax),        h(jmax),            dens_gas_gal(jmax),&
+         lnd2(jmax),        lnd3(jmax),     dens_gas_temp(jmax),  &
+         M_gas_temp(jmax),  M_gas2(jmax),   dens_gas_ana2(jmax), kappa1(jmax)
+ 
+ real*8 :: rmin,rmax,mvir,rvir, Mgal, r_half, a, Mgas, num, b2,Tcentr, kappa, lturb, k
+ 
+ real*8, dimension(jmax) :: ne(jmax),  dens_gas_paper(jmax),   temp_paper(jmax), zfe(jmax), dens_fe_paper(jmax),&
+          Zfe_paper(jmax),  M_fe(jmax), dens_star(jmax),       gradzfe(jmax), dens_fe_diff(jmax), zfe_diff(jmax), &
+          source(jmax),     Zfe_source(jmax),                  Zfe_source1(jmax),Zfe_paper_sub(jmax),&
+          M_fe_tot(jmax),   rho_fe(jmax),zfe_time(jmax),       source_time(jmax), rho_fe_time(jmax), &
+          gradzfe1(jmax),   dens_fe_source(jmax)
+       
+ integer :: n, ncicli
+ real*4,dimension(jmax) :: f_b(jmax), f_b2(jmax), f_b3(jmax)
+ real, dimension(5) :: tmax
+
+ real*8, dimension(jmax) :: Zfe_source2(jmax), Zfe_source3(jmax),Zfe_source4(jmax),Zfe_source5(jmax),&
+         zfe_diff1(jmax),   zfe_diff2(jmax),   zfe_diff3(jmax),  zfe_diff4(jmax),  zfe_diff5(jmax),  &
+         zfe1(jmax),        zfe2(jmax),        zfe3(jmax),       zfe4(jmax),       zfe5(jmax),&
+         zfe_time1(jmax),   zfe_time2(jmax),   zfe_time3(jmax),  zfe_time4(jmax),  zfe_time5(jmax)
+
+                           
+                            
+ !========= some paramenter of the problem =================0
+    densDM0=7.35d-26
+    rs=435.7*cmkpc
+    dens_gas0=2.882d-26
+    temp=8.9e7
+    rvir=2797.*cmkpc
+    fc=1.138799
+    mvir=1.3e15*msol
+    Mgas=1.d13*msol
+    Mgal=1.d12*msol
+    r_half=12*cmkpc
+    aher=r_half/(1.+sqrt(2.))
+ 
+    Zfe_sol=1.8d-3
+    Zfe_out=0.4*Zfe_sol +0.015*Zfe_sol !second term to pefectly subctract the exeed
+    zfesn=0.744/1.4
+ 
+    vturb=260.e5   
+    lturb=15.*cmkpc  
+    rscala=30.*cmkpc
+    kappa=0.11*vturb*lturb   !diffusion term  
+
+    1003 format(1f8.3, 1pe12.4) !format for bar frac on terminal
+
+    1005 format(i5, i10) !format  for i-cycle on terminal
+
+    
+ ! 
+ 
+ 
+ 
+ !__________ CREAZIONE GRGLIA _______________
+ 
+    rmin = 0.*cmkpc
+    rmax = 3000.*cmkpc
+    do j=1,jmax
+       r(j)=rmin+(j-1)*rmax/(jmax-1) 
+    enddo
+
+    do j=1,jmax-1
+       rr(j)=r(j)+0.5*(r(j+1)-r(j)) 
+    enddo
+    rr(jmax)=rr(jmax-1)+(rr(jmax-1)-rr(jmax-2)) 
+ 
+    !PROBLEMA A SIMMETRIA SFERICA
+    vol(1)=4.1888*r(1)**3 
+    do j=2,jmax
+       vol(j)=4.1888*(r(j)**3-r(j-1)**3)  !VOLUME TRA DUE SHELL  !! centrato a rr(j-1) !! definisce le shell sferiche su cui andra a calcolare masse e desnsita
+    enddo
+ 
+
+
+   
+ 
+ !____________ DISTRIBUZIONE TEORICA DENSITA DM ______________
+    do j=1,jmax       
+       x=rr(j)/rs
+       densDM(j)=densDM0/(x*(1.+x)**2) 
+    enddo
+ 
+ !_________ SOLUZIONE NUMERICA E ANALITICA MASSA DM _______________________
+ 
+    massaDM_num(1)=0.
+    massaDM_ana(1)=0.
+    do j=2,jmax
+       x=r(j)/rs
+       massaDM_num(j)=massaDM_num(j-1)+densDM(j-1)*vol(j) !DM total mass num solution
+       massaDM_ana(j)=mvir*(log(1.+x)-x/(1.+x))/fc !DM total mass ana. solution
+    enddo
+ 
+ 
+ !____________ PROFILI DI TEMPERATURA ____________
+ 
+    do j=1, jmax
+       h(j)=((rr(j)/(1.4e3*cmkpc))/0.045)
+       num1(j)=h(j)**(1.9)+0.45
+       den1(j)=h(j)**(1.9)+1
+       den2(j)=(1+(((h(j)*0.045)/0.6)**2))**(0.45) !denominatori
+ 
+       T(j)=temp*1.35*(num1(j)/den1(j))*(1/den2(j)) !temp profile for 'dens_gas_temp'
+ 
+       !valori paper per diffusione fe per confronto
+       temp_paper(j)=7*(1+((rr(j)/cmkpc)/71)**3)/(2.3+((rr(j)/cmkpc)/71)**3)*(1.16d7) !profilo temp paper rebusco
+    enddo
+ 
+    !profilo teorico massa stelle 
+    do j=1, jmax
+       M_star(j)=Mgal*r(j)**2/((r(j)+aher)**2)
+       M_tot(j)=massaDM_num(j)+M_star(j) !is only called m total
+    enddo
+   
+
+
+
+
+
+ 
+ !________________ CALCOLO DEI PROFILI DI DENSITA _____________
+ 
+    grvnfw(1)=0.
+    grav2(1)=0.
+    do j=2,jmax
+       grvnfw(j)=guniv*massaDM_num(j)/r(j)**2   
+       grav2(j)=guniv*M_tot(j)/r(j)**2  !per quando metto la galassia     
+    enddo
+ 
+    dens_gas0=5.622d-26  !original
+    dens_gas1=1.100d-25  !with bcg
+    dens_gas2=2.080d-25  !with bcg and dt/dr
+ 
+    11 continue
+    lnd(1)=log(dens_gas0)    !occhio, i logatirmni per i calcoli sono naturali
+    lnd2(1)=log(dens_gas1) 
+    lnd3(1)=log(dens_gas2)    
+
+    !_______________ STEP I INTEGRO EQUILIBRIO IDROSTATICO _______________
+    do j=2,jmax
+       gg=grvnfw(j)   !gg == G*M
+       lnd(j)=lnd(j-1)-gg*(mu*mp)*(rr(j)-rr(j-1))/(boltz*temp) !original
+ 
+       gg2=grav2(j)
+       lnd2(j)=lnd2(j-1)-grav2(j)*(mu*mp)*(rr(j)-rr(j-1))/(boltz*temp)  !per quando metto la BCG
+ 
+       Tcentr=0.5*(T(j)+T(j-1))                                          !temp centrata a r 
+       lnd3(j)=lnd3(j-1)-grav2(j)*(mu*mp)*(rr(j)-rr(j-1))/(boltz*Tcentr)-(log(T(j))-log(T(j-1)))  !con nuovo profilo di temp e BCG
+    enddo
+ 
+ 
+    !b=(8*pi*guniv*mu*mp*dens_gas0*rs**2.)/(27.*boltz*temp)
+    !A=exp(-27.*b/2.)
+    !cost=dens_gas0*A
+ 
+    !dens_gas_ana(1)=dens_gas0
+   !____________________________ STEP II, CALCOLO DELLE DENSITA + PROFILO FERRO ______________
+    do j=1,jmax
+ 
+       !a=r(j)/rs    
+       !dens_gas_ana(j)=cost*((1.+a)**(27.*b/(2.*a)) ) !densità gas analitica
+ 
+       dens_gas(j)=exp(lnd(j))        !original
+       dens_gas_gal(j)=exp(lnd2(j))   !with BCG
+       dens_gas_temp(j)=exp(lnd3(j))  !with temp profile
+ 
+       dens_star(j)=Mgal/(2.*pi)*(aher/rr(j))/(aher+rr(j))**3  !hernquish profile for fe diffusion
+ 
+       ne(j)=4.6d-2/((1+((rr(j)/cmkpc)/57)**2)**1.8) +4.8d-3/(1+((rr(j)/cmkpc)/200)**2)**0.87  !densità elettronica paper rebusco
+       dens_gas_paper(j)=1.937d-24*ne(j)     !densità gas paper rebusco
+
+ 
+       xa=rr(j)/(80.*cmkpc)
+       zfe_paper(j)=Zfe_sol*0.3*1.4*1.15*(2.2+xa**3)/(1+xa**3)/1.15  !Perseus!
+       Zfe_paper_sub(j)=Zfe_paper(j) - Zfe_out   !! subtract z_Fe,out
+
+       dens_fe_paper(j)=dens_gas_temp(j)*Zfe_paper(j)*1.4  !densità fe rebusco !da sostituire defe fe papaer per plot di confronto
+    enddo
+ 
+ 
+    !========================== MASSA GAS ==============================
+
+    M_gas(1)=vol(1)*dens_gas0
+    M_gas2(1)=vol(1)*dens_gas1
+    M_gas_temp(1)=vol(1)*dens_gas2
+
+    do j=2, jmax
+       M_gas(j)=M_gas(j-1)+dens_gas(j-1)*vol(j) !only cluster gas
+       M_gas2(j)=M_gas2(j-1)+dens_gas_gal(j-1)*vol(j) !with BCG
+       M_gas_temp(j)=M_gas_temp(j-1)+dens_gas_temp(j-1)*vol(j) !with temp profile
+    enddo
+ 
+ 
+    !==================== FRAZIONI BARIONICHE ===========================
+
+         do j=1, jmax
+         f_b(j)=(M_star(j)+M_gas(j))/(M_tot(j)+M_gas(j))
+         f_b2(j)=(M_star(j)+M_gas2(j))/(M_tot(j)+M_gas2(j))
+         f_b3(j)=(M_star(j)+M_gas_temp(j))/(M_tot(j)+M_gas_temp(j))
+         enddo
+
+
+      
+         !ciclo di controllo sulle frazioni barioniche
+         if(f_b3(4900).gt.0.172)then
+            dens_gas2=dens_gas2-1.d-27
+            go to 11
+         else if(f_b3(4900).lt.0.167)then
+            dens_gas2=dens_gas2+1.d-27
+            go to 11
+         end if
+      
+         if(f_b2(4900).gt.0.172)then
+            dens_gas1=dens_gas1-1.d-27
+            go to 11
+         else if(f_b2(4900).lt.0.168)then
+            dens_gas1=dens_gas1+1.d-27
+            go to 11
+         end if
+      
+         if(f_b(4900).gt.0.172)then
+            dens_gas0=dens_gas0-1.d-27
+            go to 11
+         else if(f_b(4900).lt.0.168)then
+            dens_gas0=dens_gas0+1.d-27
+            go to 11
+         end if
+       
+
+         write(*,*)'valori delle frazioni barioniche per controllo'
+         !
+        
+         print*, 'BARIONIC FRACTION AND DENS FOR CLUSTER GAS:'
+         write(*,1003) f_b(4900) , dens_gas0 !4900 is r_vir
+         print*, 'BARIONIC FRACTION AND DENS FOR CLUSTER GAS AND BCG:'
+         write(*,1003) f_b2(4900), dens_gas1
+         print*, 'BARIONIC FRACTION AND DENS FOR CG, BCG AND TEMP PROFILE:'
+         write(*,1003) f_b3(4900), dens_gas2
+      
+         write(*,*) !spazio sul term 
+            1033 format(1pe12.4)
+   PRINT*, 'TOTAL DARK MATTER MASS:'
+         call massa(densDM, vol)
+   PRINT*, 'TOTAL STAR MASS:'
+         WRITE(*,1033) M_star(4999)/msol
+   PRINT*, 'TOTAL GAS MASS:'
+         call massa(dens_gas_temp,vol)
+
+    !per plot di confronto
+    !____________________ SCRITTURA DEI RISULTATI _____________________
+
+    open(30, file='massaDM.dat')
+       do j=2,  jmax-1
+          write(30,1000)rr(j)/cmkpc, massaDM_ana(j)/msol, massaDM_num(j)/msol, M_star(j)/msol,&
+          M_gas_temp(j)/msol
+       enddo
+    close(30)
+    !1006 format(4(1pe12.4))
+    !dati sulle densità
+    open(10, file='densit.dat')
+    write(10,*)'#'
+    do j=1, jmax
+     write(10,1000)rr(j)/cmkpc,densDM(j), dens_gas(j), dens_gas_temp(j), dens_gas_gal(j)
+    enddo
+    close(10)
+    1000 format(6(1pe12.4))
+ 
+    !per confronto con paper rebusco
+    open(30, file='temp_paper.dat')
+    do j=1, jmax
+       write(30,1000)rr(j)/cmkpc, T(j), temp_paper(j), dens_gas_paper(j), dens_gas_temp(j)
+    enddo
+    close(30)
+ 
+    open(30, file='paper.dat')
+    do j=1, jmax
+     write(30,1000)rr(j)/cmkpc, Zfe_paper(j)/Zfe_sol, Zfe_paper_sub(j)/Zfe_sol, Zfe_out/Zfe_sol
+    enddo
+    close(30)
+
+   write(*,*)'--ncicli-------Gyr--'
+
+ !___________________________ DIFFUSION PART _______________________
+
+    !big single time cycle for source, diffusion and source with diffusion
+
+    tmax = (/2.d9*yr, 3.d9*yr, 5.d9*yr, 8.d9*yr, 1.d10*yr/)
+
+    dt=((r(5)-r(4))**2/(2*kappa))
+
+    DO i=1, 5
+        
+      tempo = 0 !we need to reset time at each i-cycle since we incorporate more phyaical events
+         
+      do j=1, jmax
+
+               !FOR SOURCE
+               Zfe_source(j)=0
+               dens_fe_source(j)=Zfe_source(j)*(dens_gas_temp(j)*1.4)
+      
+               source(j)=dens_star(j)*(6.d-23+4.7d-22)
+      
+               !FOR DIFFUSION
+               zfe_diff(j)=Zfe_paper(j)
+               dens_fe_diff(j)=dens_fe_paper(j)
+      
+               !FOR DIFFUSION + SOURCE
+               zfe(j)=0
+               rho_fe(j)=0
+         enddo
+         
+         !MAIN TYME CYCLE
+
+        do while(tempo<tmax(i))
+               !condizioni iniziali + termine sorgente
+               tempo = tempo + dt
+               ncicli = ncicli +1
+            
+      
+               do j=2, jmax-1
+                  gradzfe(j)=(zfe_diff(j)-zfe_diff(j-1))/(rr(j)-rr(j-1))
+                  gradzfe1(j)=(zfe(j)-zfe(j-1))/(rr(j)-rr(j-1))
+               enddo
+               call grad(gradzfe)
+               call grad(gradzfe1)
+
+
+         do j=2, jmax-1
+                  !----- NO DIFFUSION, ONLY SOURCE
+            dens_fe_source(j)=dens_fe_source(j)+dt*source(j) 
+
+            Zfe_source(j)=dens_fe_source(j)/(1.4*dens_gas_temp(j))
+
+
+               !----- ONLY DIFFUSION, NO SOURCE
+            densp1=0.5*(dens_gas_temp(j+1)+dens_gas_temp(j))
+            dens=0.5*(dens_gas_temp(j-1)+dens_gas_temp(j))
+         
+            dens_fe_diff(j)=dens_fe_diff(j)&
+            +(3*dt/1.4)*((r(j+1)**2*kappa*densp1*gradzfe(j+1))&
+            -(r(j)**2*kappa*dens*gradzfe(j)))/(r(j+1)**3-r(j)**3)
+         
+            zfe_diff(j)=dens_fe_diff(j)/(1.4*dens_gas_temp(j))
+
+
+               !----- DIFFUSION + SOURCE
+            rho_fe(j)=rho_fe(j)+dt*source(j)
+
+            rho_fe(j)=rho_fe(j)&
+            +(3*dt/1.4)*((r(j+1)**2*kappa*densp1*gradzfe1(j+1))&
+            -(r(j)**2*kappa*dens*gradzfe1(j)))/(r(j+1)**3-r(j)**3)
+
+            zfe(j)=rho_fe(j)/(1.4*dens_gas_temp(j))
+         enddo
+
+         call BCb(dens_fe_source)    !boundary conditions 
+         call BCb(Zfe_source)   
+         call BCb(zfe_diff) 
+         call BCb(dens_fe_diff)        
+         call BCb(rho_fe)
+         call BCb(zfe)
+      
+             
+      enddo !end of time cycle
+
+         do j=1, jmax
+            zfe_diff(j)=zfe_diff(j)-Zfe_out !sottraggo eccesso
+            dens_fe_diff = zfe_diff(j)*dens_gas_temp(j)*1.4 !per il calcolo della massa SENZA ECCESSO
+         enddo
+
+        !_________________- MAIN RESULTS-___________________
+        do j=1, jmax-1
+         if(i==1) then
+            Zfe_source1(j)=Zfe_source(j)
+            zfe_diff1(j) = zfe_diff(j)
+            zfe1(j) = zfe(j)   
+             
+         end if
+         if(i==2) then
+            Zfe_source2(j)=Zfe_source(j)
+            zfe_diff2(j) = zfe_diff(j)
+            zfe2(j) = zfe(j)  
+                    
+         end if
+         if(i==3) then
+            Zfe_source3(j)=Zfe_source(j)
+            zfe_diff3(j) = zfe_diff(j)
+            zfe3(j) = zfe(j)  
+                  
+         end if
+         if(i==4) then
+            Zfe_source4(j)=Zfe_source(j)
+            zfe_diff4(j) = zfe_diff(j)
+            zfe4(j) = zfe(j)   
+                  
+         end if
+         if(i==5) then
+            Zfe_source5(j)=Zfe_source(j)
+            zfe_diff5(j) = zfe_diff(j)
+            zfe5(j) = zfe(j)  
+           
+         end if
+      enddo
+         
+         write(*,1005) ncicli, int(tempo/(1.d9*yr)) !in order to see the evolution over the i cycle
+
+   ENDDO !end of i cycle
+
+   do j=2, jmax-1
+      dens_fe_paper = Zfe_paper_sub(j)*dens_gas_temp(j)*1.4  !ricalcolo densità con eccesso tolto per il calcolo della massa
+   enddo
+
+   write(*,*) 'massa prima diffusione'
+      call massa(dens_fe_paper, vol)
+
+   write(*,*)'massa dopo diffusione'
+      call massa(dens_fe_diff, vol)
+
+   write(*,*)'massa prodotta da sorgente'
+      call massa(dens_fe_source,vol)
+
+
+   
+
+       open(30, file='sorgente.dat')
+       write(30,*)'#_--rr[kpc]--zfe_sub/sol----zfe/sol-----t=2 Gyr------t=3 Gyr-----t=5 Gyr-----t=8 Gyr-----t=10 Gyr------'
+      do j=1, jmax-2
+         write(30, 1002) rr(j)/cmkpc, Zfe_paper_sub(j)/Zfe_sol, Zfe_paper(j)/Zfe_sol, Zfe_source1(j)/Zfe_sol,&
+         Zfe_source2(j)/Zfe_sol, Zfe_source3(j)/Zfe_sol,Zfe_source4(j)/Zfe_sol,Zfe_source5(j)/Zfe_sol
+       enddo
+      close(30)
+      1002 format (12(1pe12.4))
+
+      open(30, file='diffusione.dat')
+      write(30,*)'#_--rr[kpc]--zfe_sub/sol----zfe/sol-----t=2 Gyr------t=3 Gyr-----t=5 Gyr-----t=8 Gyr-----t=10 Gyr------'
+      do j=1, jmax-2
+         write(30, 1002) rr(j)/cmkpc, Zfe_paper_sub(j)/Zfe_sol, Zfe_paper(j)/Zfe_sol, zfe_diff1(j)/Zfe_sol, &
+         zfe_diff2(j)/Zfe_sol, zfe_diff3(j)/Zfe_sol, zfe_diff4(j)/Zfe_sol, zfe_diff5(j)/Zfe_sol
+      enddo
+      close(30)
+
+      open(30, file='diff_sorg.dat')
+      write(30,*)'#_--rr[kpc]--zfe_sub/sol----zfe/sol-----t=2 Gyr------t=3 Gyr-----t=5 Gyr-----t=8 Gyr-----t=10 Gyr------'
+      do j=1, jmax-2
+         write(30, 1002) rr(j)/cmkpc, Zfe_paper_sub(j)/Zfe_sol, Zfe_paper(j)/Zfe_sol, zfe1(j)/Zfe_sol, &
+         zfe2(j)/Zfe_sol, zfe3(j)/Zfe_sol, zfe4(j)/Zfe_sol, zfe5(j)/Zfe_sol
+      enddo
+      close(30)
+    
+
+ ! test variazione di parametri per riprodurre osservazioni
+     
+      tempo = 0 !time reset after the first tyme cycle
+ 
+      tnow = 13.7d9*yr
+      kappa = 9.95d28
+      dt=((r(5)-r(4))**2/(2*kappa))
+      
+
+      do i=1, 5
+         !definizione parametri su cui lavoro
+         zfe_time = 0   !parto da abbondanza = 0
+         rho_fe_time = 0
+         tempo = 0 !time reset for i-cycle
+
+         do while(tempo<tmax(i))
+
+            tempo = tempo+dt
+            
+            snu = 0.02*((tempo/(tnow))**(-1.1))
+
+            do j=2, jmax-1
+
+               alphast=4.7e-20
+               alphasn = (0.74/7.5)*(1/1.d10)*(1/3.156d9)*snu
+      
+               source_time(j)=dens_star(j)*(alphast*Zfe_sol/1.4+ alphasn)
+            enddo
+
+            do j=2, jmax-1
+               gradzfe(j)=(zfe_time(j)-zfe_time(j-1))/(rr(j)-rr(j-1))
+            enddo
+            gradzfe(1)=0
+            gradzfe(jmax)=0 
+      
+            
+      
+            do j=2, jmax-1
+               if(r(j)>60.*cmkpc)then
+                  kappa = 2.15d29
+                  dt=((r(5)-r(4))**2/(2*kappa))
+               else if(r(j)<=60.*cmkpc)then
+                  kappa = 9.95d28
+                  dt=((r(5)-r(4))**2/(2*kappa))
+               end if
+
+
+               rho_fe_time(j)=rho_fe_time(j)+dt*source_time(j)
+
+               densp1=0.5*(dens_gas_temp(j+1)+dens_gas_temp(j))
+               dens=0.5*(dens_gas_temp(j-1)+dens_gas_temp(j))
+         
+               rho_fe_time(j)=rho_fe_time(j)&
+               +(3*dt/1.4)*((r(j+1)**2*kappa*densp1*gradzfe(j+1))&
+               -(r(j)**2*kappa*dens*gradzfe(j)))/(r(j+1)**3-r(j)**3)
+         
+               zfe_time(j)=rho_fe_time(j)/(1.4*dens_gas_temp(j))
+            enddo
+
+            call BCb(rho_fe_time)
+            call BCb(zfe_time)
+
+         enddo !end of time cycle
+
+                 !_________________- MAIN RESULTS-___________________
+        do j=1, jmax-1
+         if(i==1) then
+           zfe_time1(j) = zfe_time(j)         
+         end if
+         if(i==2) then
+            zfe_time2(j) = zfe_time(j)        
+         end if
+         if(i==3) then
+            zfe_time3(j) = zfe_time(j)         
+         end if
+         if(i==4) then
+            zfe_time4(j) = zfe_time(j)        
+         end if
+         if(i==5) then
+            zfe_time5(j) = zfe_time(j)       
+         end if
+      enddo
+      enddo !end of i  cycle
+
+      write(*,*)'massa con snu variabile'
+      call massa(rho_fe_time,vol)
+
+      open(30, file='time.dat')
+      write(30,*)'#_--rr[kpc]--zfe_sub/sol----zfe/sol-----t=2 Gyr------t=3 Gyr-----t=5 Gyr-----t=8 Gyr-----t=10 Gyr------'
+      do j=1, jmax-2
+         write(30, 1002) rr(j)/cmkpc, Zfe_paper_sub(j)/Zfe_sol, Zfe_paper(j)/Zfe_sol, zfe_time1(j)/Zfe_sol, &
+         zfe_time2(j)/Zfe_sol, zfe_time3(j)/Zfe_sol, zfe_time4(j)/Zfe_sol, zfe_time5(j)/Zfe_sol
+      enddo
+      close(30)
+
+      
+   
+
+
+
+  end program progetto1
+
+!subroutines
+
+   SUBROUTINE BCb(z2) ! BC di outflow tradizionali
+        USE DATA
+        IMPLICIT NONE
+        REAL*8, DIMENSION (jmax):: z2
+        
+        z2(1)=z2(2)
+        z2(jmax)=z2(jmax-1)
+   END SUBROUTINE BCb
+
+   SUBROUTINE grad(z1) 
+      USE DATA
+      IMPLICIT NONE
+      REAL*8, DIMENSION (jmax):: z1
+      
+      z1(1)=0
+      z1(jmax)=0
+   END SUBROUTINE grad
+
+   subroutine massa(a1, b1)
+      use data
+      implicit none 
+      integer :: j
+      real*8, dimension(jmax):: a1, b1, mass
+      real*8 :: result
+
+      do j=2, jmax
+      mass(j)=a1(j)*b1(j-1)
+      enddo
+
+     
+      result=sum(mass)
+      
+
+      write(*,89) real(result/msol)
+      
+      89 format(1pe12.4)
+   end subroutine massa
+
+ 
+
+
+     
--- relazioni/hydro.jl
+++ relazioni/hydro.jl
@@ -1,7 +1,10 @@
-﻿
+﻿using GLMakie
+using ProgressMeter
+
+
 function main()
 #definizione de parametri
-N = 2000
+N = 1000
 cmpc = 3.085e18
 mu = 1.4
 msol = 1.989e33
@@ -12,8 +15,11 @@ yr = 3.156e7
 
 
 
+
 xa = zeros(N)
 xb = copy(xa)
+dxa = copy(xa)
+dxb = copy(xa)
 
 
 #grid creation
@@ -36,15 +42,27 @@ for i=2:N-1
 end
 xb[N] = xb[N-1]+(xb[N-1]-xb[N-2])
 
+for i in 2:N-1
+    dxa[i] = xa[i+1] - xa[i] # delta spaziale
+    dxb[i] = xb[i] - xb[i-1]
+end
+dxa[1] = dxa[2]
+dxa[N] = dxa[N-1]
+dxb[1] = dxb[2]
+dxb[N] = dxb[N-1]
+
+
+
+
 
 
 
 #fattori di scala
 g2a = zeros(N)
-g2b = rand(N)
-g31a = rand(N)
-g31b = rand(N)
-dvl1a = rand(N)
+g2b = zeros(N)
+g31a = zeros(N)
+g31b = zeros(N)
+dvl1a = zeros(N)
 dvl1b = copy(g2a)
 
 sdr = 1
@@ -69,7 +87,7 @@ end
 
 #----------- fattori supernova e condizioni iniziali --------
 e0 = 1e51
-c2 = 3
+C2 = 3
 cfl = 0.01
 
 d0 = 2e-24
@@ -85,6 +103,16 @@ t = copy(v)
 p = copy(v)
 q = copy(v)
 divV = copy(v)
+F3 = copy(v)
+
+q = zeros(N)
+s = copy(v)
+dstar = copy(v)
+F1 = copy(v)
+M = copy(v)
+e_dstar = copy(v)
+vstar = copy(v)
+F2 = copy(v)
 
 for i=1:N
     v[i] = 0
@@ -105,12 +133,33 @@ tmax=1e5*yr
 tempo = 0. 
 ncicli = 0
 bonobo = 0
+winds = false
 
 display("Initial condition")
+display(xb[1])
 display(v[2:4])
 display(d[2:4])
 display(t[2:4])
 display(e[2:4]/e0)
+
+
+fig = Figure()
+ax = Axis(fig[1,1],
+    title= "hydro",
+    xlabel=" distance",
+    ylabel = "density",
+    
+    #xaxis = :log10,
+    #yaxis = :log10
+    )
+display(fig)
+
+plot = lines!(ax, xb, d)
+
+
+println("Hit enter when plot is ready")
+readline()
+
 
 
 #--------------- CICLO TEMPORALE PRIMCIPALE ----------------
@@ -120,6 +169,7 @@ while tempo <tmax
     #global ncicli
     #global tempo
     #global gam, cfl, deltax, yr
+    #println(ncicli)
 
     ncicli += 1
     bonobo += 1
@@ -137,62 +187,67 @@ while tempo <tmax
 
     #definisco dtmin
     dtmin = 1e30
-    for i=2:N-1        
-        dtmin = min(dtmin, (xb[i]-xb[i-1])/(abs(v[i])+sqrt(gam*p[i]/d[i])))
-    end
-
-    dtmin = cfl*dtmin
-    tempo += dtmin
-
-    if ncicli == 2
-        println("first time step = ", dtmin/yr)
-        println("initial velocity = ", v[4]/1e5)
-        #println(gam,yr,cfl)
-    elseif ncicli > 1000
-        display("cocco bello")
-        break
-    end
-    end
-
-    #------------ SOURCE STEP --------------
     for i=2:N-1
-        v[i] = v[i]-dtmin*2*(p[i]-p[i-1])/((d[i]+d[i-1]))*deltax
-        if v[i]<0
-            v[i] = 0
+        dtmin=min(dtmin,(xb[i]-xb[i-1])/(abs(v[i])+sqrt(gam*p[i]/d[i])))
+    end
+    
+    dtmin=cfl*dtmin #cfl is the C constant 
+    
+    tempo+=dtmin
+    
+    if ncicli == 2
+        println("FIRST TIME-STEP == ", dtmin/yr)
+        println("initial energy ==", e[2]*vol)
+        if winds == false
+            println("initial velocity ==", v[4]/1e5)
+            #println("expected temperature")
+            #write(*,1034)  3*mu*mp/(16*kbol)*v(4)**2
+        else
+            println("initial velocity ==", v[3]/1e5)
         end
     end
-    v[1]=v[2]
-    v[N]=v[N-1]
+    
 
-    #artificial viscosity
+    #------------ SOURCE STEP --------------
+    #1 AGGIORNAMENTO DELLE VELOITA
     for i=2:N-1
-        if v[i+1]-v[i]<0 
-            q[i] = c2*d[i]*(v[i+1]-v[i])^2
+        v[i] = v[i] - dtmin*2*(p[i]-p[i-1])/((d[i]+d[i-1])*dxb[i])
+        if v[i] < 1e-20
+            v[i] = 0
+        end
+        
+    end
+    v[1] = v[2]
+    v[N] = v[N-1]
+    
+    #2 ARTIFICIAL VISCOSITY
+    for i=2:N-1
+        if v[i+1] - v[i] < 0
+            q[i] = C2*d[i]*(v[i+1] - v[i])^2
         else
             q[i] = 0
         end
     end
     q[1] = q[2]
     q[N] = q[N-1]
-
-
-        # SUBSTEP II: AGGIORNAMENTO DELLA VELOCITÃ FOR ARTIFICIAL VISC. [as additional term to pression] AND ENERGY
-        for i=2: N-1
-            v[i]=v[i]-dtmin*2*(q[i]-q[i-1])/((d[i]+d[i-1])*deltax)
-            if v[i]<1e-20
-                v[i]=0.
-            end
+    
+    #AGG VEL CON VISCOSITA
+    for i=2:N-1
+        v[i] = v[i] - dtmin*2*(q[i]-q[i-1])/((d[i]+d[i-1])*dxb[i])
+        if v[i] < 1e-20
+            v[i] = 0
         end
-        v[1] = v[2]
-        v[N] = v[N-1]
-       
-
-        # SUBSTEP II: ENERGY 
-        for i=2: N-1
-            e[i]=e[i]-dtmin*q[i]*(v[i+1]-v[i])/deltax
-        end
-        e[1] = e[2]
-        e[N] = e[N-1]
+    end
+    v[1]=v[2]
+    v[N] = v[N-1]
+    
+    #ENERGIA
+    for i=2:N-1
+        e[i] = e[i] - dtmin*q[i]*(v[i+1]-v[i])/dxa[i]
+    end
+    e[1] = e[2]
+    e[N] = e[N-1]
+    
 
         #SUBSTEP III: COMPRESSION HEATING
         for i=2:N-1
@@ -207,10 +262,134 @@ while tempo <tmax
         end
         e[1] = e[2]
         e[N] = e[N-1]
+        
+        for i=2: N-1
+            t[i] = e[i]/(cv*d[i])
+            if t[i] < t0_min
+                t[i] = t0_min
+            end
+        end
 
-        println("se stai leggendo questo sei uhn bomber")
+        for i=2: N-1
+            e[i]=cv*d[i]*t[i]
+        end
+        e[1] = e[2]
+        e[N] = e[N-1]
 
-end #fine della funzione
+                # Define the momentum density
+        for i = 2:N-1
+            s[i] = 0.5*(d[i] + d[i-1])*v[i]   # at "i"
+        end
+        s[1] = s[2]
+        s[N] = s[N-1]
+
+
+        # Update the density based on the velocity (upwind)
+        for i = 2:N-1
+            if v[i] > 0
+                dstar[i] = d[i-1]   # at "i"
+            else
+                dstar[i] = d[i]
+            end
+        end
+        dstar[N] = dstar[N-1]
+        dstar[1] = dstar[3]
+
+        for i = 2:N
+            F1[i] = dstar[i]*v[i]*g2a[i]*g31a[i]   # density flux across surfaces at "i"
+        end
+
+        # Update the energy: calculate the surface momentum and energy on cell edges
+        for i = 2:N-1
+            M[i] = dstar[i]*v[i]   # surface momentum
+        end
+        M[1] = M[2]
+        M[N] = M[N-1]
+
+        for i = 2:N-1
+            if v[i] > 0
+                e_dstar[i] = e[i-1]/d[i-1]   # at "i"
+            else
+                e_dstar[i] = e[i]/d[i]
+            end
+        end
+        e_dstar[N] = e_dstar[N-1]
+        e_dstar[1] = e_dstar[3]
+
+        # Calculate the density flux across surfaces
+
+        # Update the density after the arrival of the matter flow
+        for i = 2:N-1
+            d[i] = d[i] - dtmin*(F1[i+1] - F1[i])/dvl1a[i]   # density after the arrival of matter flow
+        end
+        d[1] = d[2]
+        d[N] = d[N-1]
+
+        # Calculate the energy flux
+        for i = 2:N
+            F2[i] = e_dstar[i]*M[i]*g2a[i]*g31a[i]   # energy flux
+        end
+        F2[1] = F2[2]
+        F2[N] = F2[N-1]
+
+        # Update the energy after the arrival of the energy flow
+        for i = 2:N-1
+            e[i] = e[i] - dtmin*(F2[i+1] - F2[i])/dvl1a[i]   # energy after the arrival of energy flow
+        end
+        e[1] = e[2]
+        e[N] = e[N-1]
+
+        #-------------------
+
+        for i=2:N-1
+            if ((v[i-1]+v[i])*0.5 > 0)
+                vstar[i] = v[i-1]  # at i-1/2
+            else
+                vstar[i] = v[i]
+            end
+        end
+        vstar[1] = vstar[2]
+        vstar[N] = vstar[N-1]
+        
+        for i=1:N-1
+            F3[i] = vstar[i+1] * 0.5 * (M[i] + M[i+1]) * g2b[i] * g31b[i]  # at i+1/2
+        end
+        
+        for i=2:N-1
+            s[i] = s[i] - dtmin / dvl1b[i] * (F3[i] - F3[i-1])  # update momentum density
+        end
+        s[1] = s[2]
+        s[N] = s[N-1]
+        
+        for i=2:N-1
+            v[i] = 2. * s[i] / (d[i] + d[i-1])  # final velocity of the cells
+        end
+        v[1] = v[2]
+        v[N] = v[N-1]
+        
+        if (cfl < 0.5)
+            cfl = cfl + (cfl * 0.1)
+        else
+            cfl = 0.5
+        end
+        
+        #if orangotango == true
+            #println("TIME PASSED == $(tempo/yr), $ncicli")
+            
+            delete!(plot.parent, plot)
+            plot = lines!(ax,xb,d)
+            sleep(0.02)
+        #end
+
+        #delete!(makie_plot_3d_contour.parent, makie_plot_3d_contour)
+
+
+
+
+        #println("se stai leggendo questo sei uhn bomber")
+
+end 
+end
 main()
 
 
